fa572dc121bbba2840dcd6c1a59501f8e3c0fb80
Funmito <funmito.odefemi@outlook.com>
2018-07-01 07:23:52 +0000

    feat: Add landing page for ofunmito.github.io

--- js/script.js
+++ js/script.js
@@ -1,5 +1,5 @@
 // contact message    
-var myMessage = document.querySelector("#sendMessage");
+const myMessage = document.querySelector("#sendMessage");
 myMessage.addEventListener('click', function(evt) {
 
     alert('Thank you for getting in touch. We will get back to you soon')
@@ -7,8 +7,19 @@ myMessage.addEventListener('click', function(evt) {
 });
 
 // copyright year
-var newDate = new Date();
-var dateString = "";
+const newDate = new Date();
+const dateString = "";
 // get current year
 dateString += newDate.getFullYear();
 $('#currentYear').text(dateString);
+
+
+/* Toggle "is-responsive" class to navigation when the user clicks on the icon */
+function addResponsiveness() {
+    const x = document.getElementById("pageNavbar");
+    if (x.className === "navigation") {
+        x.className += " is-responsive";
+    } else {
+        x.className = "navigation";
+    }
+}
