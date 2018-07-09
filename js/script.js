ec34042ebc35dd92c087db819723ca7bebd80897
Funmito <funmito.odefemi@outlook.com>
2018-07-09 14:45:58 +0000

    Add footer

--- js/script.js
+++ js/script.js
@@ -1,14 +1,6 @@
-// contact message    
-const myMessage = document.querySelector("#sendMessage");
-myMessage.addEventListener('click', function(evt) {
-
-    alert('Thank you for getting in touch. We will get back to you soon')
-    console.log(sendMessage);
-});
-
 // copyright year
-const newDate = new Date();
-const dateString = "";
+let newDate = new Date();
+let dateString = "";
 // get current year
 dateString += newDate.getFullYear();
 $('#currentYear').text(dateString);
