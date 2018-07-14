c2a3bddb55ff66e0bae6e934101c9bbd63149f7d
ofunmito <funmito.odefemi@outlook.com>
2018-07-14 19:14:29 +0100

    Add placeholders for image

--- doyin-femi/js/script.js
+++ doyin-femi/js/script.js
@@ -4,3 +4,32 @@ let dateString = "";
 // get current year
 dateString += newDate.getFullYear();
 $('#currentYear').text(dateString);
+
+// Slider
+var slideInterval = 3500;
+
+function getFigures() {
+    return document.getElementById('carousel').getElementsByTagName('figure');
+}
+
+function moveForward() {
+    var pointer;
+    var figures = getFigures();
+    for (var i = 0; i < figures.length; i++) {
+        if (figures[i].className == 'visible') {
+            figures[i].className = '';
+            pointer = i;
+        }
+    }
+    if (++pointer == figures.length) {
+        pointer = 0;
+    }
+    figures[pointer].className = 'visible';
+    setTimeout(moveForward, slideInterval);
+}
+
+function startPlayback() {
+    setTimeout(moveForward, slideInterval);
+}
+
+startPlayback();
