823a3150a8079bffe5f79a0d94185ff354eaa992
Funmito <funmito.odefemi@outlook.com>
2018-09-27 15:19:08 +0100

    test: Add test for menu hidden on page load

--- udacity-fe-nanodegree/feedreader-test/jasmine/spec/feedreader.js
+++ udacity-fe-nanodegree/feedreader-test/jasmine/spec/feedreader.js
@@ -60,8 +60,8 @@ $(function() {
          * the CSS to determine how we're performing the
          * hiding/showing of the menu element.
          */
-        it('menu hidden', function() {
-
+        it('menu is hidden', function() {
+            expect($('body').hasClass('menu-hidden')).toBe(true);
         });
 
         /* TODO: Write a test that ensures the menu changes
@@ -69,8 +69,8 @@ $(function() {
          * should have two expectations: does the menu display when
          * clicked and does it hide when clicked again.
          */
-        it('menu change', function() {
-
+        it('menu changes', function() {
+            // expect($('.menu-icon-link').);
         });
 
     });
