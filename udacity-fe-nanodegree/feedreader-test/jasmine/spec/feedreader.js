8d805f091ed5b31131af464d000a385d04af7e04
Funmito <funmito.odefemi@outlook.com>
2018-09-28 16:24:03 +0100

    test: Update test

--- udacity-fe-nanodegree/feedreader-test/jasmine/spec/feedreader.js
+++ udacity-fe-nanodegree/feedreader-test/jasmine/spec/feedreader.js
@@ -87,13 +87,14 @@ $(function() {
          * Remember, loadFeed() is asynchronous so this test will require
          * the use of Jasmine's beforeEach and asynchronous done() function.
          */
+        let feed = document.querySelector('.feed');
+
         beforeEach(function(done) {
             loadFeed(0, done);
         });
 
         // spec
         it('has at least one entry', function(done) {
-            let feed = document.querySelector('.feed');
             expect(feed.children.length > 0).toBe(true);
             done();
         });
