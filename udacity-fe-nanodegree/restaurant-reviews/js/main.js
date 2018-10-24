5eb429dfdf827417731a0f5532ce2cddcf325817
Funmito <funmito.odefemi@outlook.com>
2018-10-24 17:23:39 +0100

    add aria-role and tabIndex to index.html

--- udacity-fe-nanodegree/restaurant-reviews/js/main.js
+++ udacity-fe-nanodegree/restaurant-reviews/js/main.js
@@ -177,6 +177,7 @@ createRestaurantHTML = (restaurant) => {
 
     const more = document.createElement('a');
     more.innerHTML = 'View Details';
+    more.tabIndex = '2';
     more.href = DBHelper.urlForRestaurant(restaurant);
     li.append(more)
 
