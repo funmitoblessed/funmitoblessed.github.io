92a2097f153b03ad54ebbc031de547a120e29126
Funmito <funmito.odefemi@outlook.com>
2018-10-26 00:34:05 +0100

    feat: add fetch event listener to sw

--- udacity-fe-nanodegree/restaurant-reviews/sworker.js
+++ udacity-fe-nanodegree/restaurant-reviews/sworker.js
@@ -1,8 +1,22 @@
 const cacheThese = [
     '/udacity-fe-nanodegree/restaurant-reviews',
     'index.html',
-    'restaurant.html'
-
+    'restaurant.html',
+    'css/styles.css',
+    'data/restaurants.json',
+    'img/1.jpg',
+    'img/2.jpg',
+    'img/3.jpg',
+    'img/4.jpg',
+    'img/5.jpg',
+    'img/6.jpg',
+    'img/7.jpg',
+    'img/8.jpg',
+    'img/9.jpg',
+    'img/10.jpg',
+    'js/dbhelper.js',
+    'js/main.js',
+    'js/restaurant_info.js'
 ]
 
 self.addEventListener('install', function(event) {
@@ -10,6 +24,25 @@ self.addEventListener('install', function(event) {
     event.waitUntil(
         caches.open('app-v1').then(function(cache) {
             return cache.addAll(cacheThese);
+        })
+    );
+
+});
+
+
+self.addEventListener('fetch', function(event) {
+
+    event.respondWith(
+        caches.match(event.request).then(function(response) {
+            return response || fetch(event.request);
+            console.log(response);
+            // if (response) {
+            //     console.log('Found!!! , event.request);
+            //     return response;
+            // } else {
+            //     console.log('Some event not Found in cache, Now Fetching!...');
+            //     return fetch(event.request);
+            // }
         })
     );
 
