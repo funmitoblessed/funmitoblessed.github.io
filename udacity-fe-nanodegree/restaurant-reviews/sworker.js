704823eb615e7b8471691942c3212584679ba8af
Funmito <funmito.odefemi@outlook.com>
2018-10-26 02:00:58 +0100

    update path

--- udacity-fe-nanodegree/restaurant-reviews/sworker.js
+++ udacity-fe-nanodegree/restaurant-reviews/sworker.js
@@ -1,23 +1,23 @@
 const cacheThese = [
     'https://unpkg.com/leaflet@1.3.1/dist/leaflet.js',
     'https://unpkg.com/leaflet@1.3.1/dist/leaflet.css',
-    '/udacity-fe-nanodegree/restaurant-reviews/index.html'
-    // './udacity-fe-nanodegree/restaurant-reviews/restaurant.html',
-    // './udacity-fe-nanodegree/restaurant-reviews/css/styles.css',
-    // './udacity-fe-nanodegree/restaurant-reviews/data/restaurants.json',
-    // './udacity-fe-nanodegree/restaurant-reviews/img/1.jpg',
-    // './udacity-fe-nanodegree/restaurant-reviews/img/2.jpg',
-    // './udacity-fe-nanodegree/restaurant-reviews/img/3.jpg',
-    // './udacity-fe-nanodegree/restaurant-reviews/img/4.jpg',
-    // './udacity-fe-nanodegree/restaurant-reviews/img/5.jpg',
-    // './udacity-fe-nanodegree/restaurant-reviews/img/6.jpg',
-    // './udacity-fe-nanodegree/restaurant-reviews/img/7.jpg',
-    // './udacity-fe-nanodegree/restaurant-reviews/img/8.jpg',
-    // './udacity-fe-nanodegree/restaurant-reviews/img/9.jpg',
-    // './udacity-fe-nanodegree/restaurant-reviews/img/10.jpg',
-    // './udacity-fe-nanodegree/restaurant-reviews/js/dbhelper.js',
-    // './udacity-fe-nanodegree/restaurant-reviews/js/main.js',
-    // './udacity-fe-nanodegree/restaurant-reviews/js/restaurant_info.js'
+    '/udacity-fe-nanodegree/restaurant-reviews/index.html',
+    '/udacity-fe-nanodegree/restaurant-reviews/restaurant.html',
+    '/udacity-fe-nanodegree/restaurant-reviews/css/styles.css',
+    '/udacity-fe-nanodegree/restaurant-reviews/data/restaurants.json',
+    '/udacity-fe-nanodegree/restaurant-reviews/img/1.jpg',
+    '/udacity-fe-nanodegree/restaurant-reviews/img/2.jpg',
+    '/udacity-fe-nanodegree/restaurant-reviews/img/3.jpg',
+    '/udacity-fe-nanodegree/restaurant-reviews/img/4.jpg',
+    '/udacity-fe-nanodegree/restaurant-reviews/img/5.jpg',
+    '/udacity-fe-nanodegree/restaurant-reviews/img/6.jpg',
+    '/udacity-fe-nanodegree/restaurant-reviews/img/7.jpg',
+    '/udacity-fe-nanodegree/restaurant-reviews/img/8.jpg',
+    '/udacity-fe-nanodegree/restaurant-reviews/img/9.jpg',
+    '/udacity-fe-nanodegree/restaurant-reviews/img/10.jpg',
+    '/udacity-fe-nanodegree/restaurant-reviews/js/dbhelper.js',
+    '/udacity-fe-nanodegree/restaurant-reviews/js/main.js',
+    '/udacity-fe-nanodegree/restaurant-reviews/js/restaurant_info.js'
 ]
 
 self.addEventListener('install', function(event) {
